cf1534e33e1153962292a7bc6d57ee78e028b004
Yusuf Umar Faruq <111466842+Luwa-Tech@users.noreply.github.com>
2025-01-15 07:59:56 +0000

    queue data structure

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,49 @@
+type node<T> = {
+    value: T,
+    next?: node<T>
+}
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head: node<T> | undefined;
+    private tail: node<T> | undefined;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        const node = { value: item } as node<T>;
+        this.length++;
 
-}
+        if (!this.tail) {
+            this.head = this.tail = node;
+            return;
+        }
+
+        this.tail.next = node;
+        this.tail = node;
+    }
+
     deque(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        this.length--;
+
+        const head = this.head;
+        this.head = this.head.next;
+
+        if (this.length === 0) {
+            this.head = this.tail = undefined;
+        }
+
+        return head.value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
